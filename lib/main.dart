08b61789b15ee1c47aaafa623af0319bdff38a31
Mansour <mnasor3000@outlook.sa>
2022-11-08 14:13:53 +0300

    first Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int count = 0;
+  int currentIndex = 0;
 
   String buttonName = 'Click';
 
@@ -25,17 +26,49 @@ class _MyAppState extends State<MyApp> {
           title: const Text("App Title"),
         ),
         body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              setState(() {
-                buttonName = 'Click';
-                count++;
-                buttonName = buttonName + '$count';
-              });
-            },
-            child: Text(
-              buttonName,
-              style: TextStyle(fontSize: 20),
+          child: currentIndex==0? Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.black,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    onPrimary: Colors.black,
+                    primary: Colors.red,
+                  ),
+                  
+                  onPressed: () {
+                    setState(() {
+                      buttonName = 'Click';
+                      count++;
+                      buttonName = buttonName + '$count';
+                    });
+                  },
+                  child: Text(
+                    buttonName,
+                    style: TextStyle(fontSize: 0),
+                  ),
+                  
+                ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonName = 'Click';
+                      count++;
+                      buttonName = buttonName + '$count';
+                    });
+                  },
+                  child: Text(
+                    buttonName,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -49,11 +82,15 @@ class _MyAppState extends State<MyApp> {
               label: 'Settings',
               icon: Icon(
                 Icons.settings,
-                color: Colors.blue,
-                size: 40.0,
               ),
             )
           ],
+          currentIndex: 0,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
         ),
       ),
     );
